456a509473990494738011fc736908073602bf08
Anton Asmirko <AntonAsmirko@yandex.ru>
2020-12-16 10:40:12 +0300

    some fixes

--- app/src/main/java/com/example/fakeahpeeclient/ui/behaviors/SwipeRefreshLayoutBehavior.kt
+++ app/src/main/java/com/example/fakeahpeeclient/ui/behaviors/SwipeRefreshLayoutBehavior.kt
@@ -5,6 +5,8 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.math.MathUtils
 import androidx.core.view.*
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -35,15 +37,16 @@ class SwipeRefreshLayoutBehavior(context: Context, attrs: AttributeSet?) :
         consumed: IntArray,
         type: Int
     ) {
-        val params = child.layoutParams as CoordinatorLayout.LayoutParams
-        params.setMargins(
-            child.marginLeft,
-            MathUtils.clamp(child.marginTop - dy, 0, 150),
-            child.marginRight,
-            child.marginBottom
-        )
-        child.layoutParams = params
-        prevDy = dy
+//        val params = child.layoutParams as CoordinatorLayout.LayoutParams
+//        params.setMargins(
+//            child.marginLeft,
+//            MathUtils.clamp(child.marginTop - dy, 0, 150),
+//            child.marginRight,
+//            child.marginBottom
+//        )
+//        child.layoutParams = params
+//        prevDy = dy
+        child.translationY = MathUtils.clamp(child.translationY + dy, 0f, 150f)
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
     }
 }
